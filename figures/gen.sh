d0e957873907d5e96c386778dd591b14f75e5edb
Benjamin Nisker <Benjamin-nisker@hotmail.com>
2022-04-22 12:50:38 +0200

    fix big circ

--- figures/gen.sh
+++ figures/gen.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 mkdir exports
-find . -name "*.drawio" -exec rm -f {}.pdf \; -exec sh -c 'drawio --crop -x -o "exports/${0%.drawio}.pdf" {}' {} \;
+find . -name "*.drawio" -exec rm -f {}.pdf \; -exec sh -c 'drawio --crop -x -o "${0%.drawio}.pdf" {}' {} \;
+mv *.pdf exports/
