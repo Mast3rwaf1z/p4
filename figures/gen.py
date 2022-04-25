370087c6243f5142d6d63218ad518887c2266d44
Thomas Jensen <thomasmollerjensen@live.dk>
2022-04-25 09:46:05 +0200

    added multiprocessing gen script

---
+++ figures/gen.py
@@ -0,0 +1,18 @@
+from multiprocessing import Process
+from os import system
+from glob import glob
+
+def exec():
+    system("mkdir exports")
+    drawio_files = glob("*.drawio")
+    system("rm exports/*.pdf")
+    processes = list()
+    for i in range(len(drawio_files)):
+        name = drawio_files[i][:len(drawio_files[i])-7] + ".pdf"
+        processes.append(Process(target= lambda: system(f'drawio --crop -x -o {name} {drawio_files[i]}')))
+        processes[len(processes)-1].start()
+    for p in processes:
+        p.join()
+    system("mv *.pdf exports/")
+
+exec()
