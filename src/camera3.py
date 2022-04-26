88722a91003acf40c9e5be4792dcd4b3ea4b6678
Rasmus Davidsen <rdavid20@student.aau.dk>
2022-04-26 10:22:50 +0200

    test 2

--- src/camera3.py
+++ src/camera3.py
@@ -1,6 +1,11 @@
 import time
 import picamera
+import picamera.array
+import numpy as np
 
 with picamera.PiCamera() as camera:
-    camera.resolution = (2592, 1944)
-    camera.capture('image.data', 'rgb')
+    camera.resolution = (2592, 1952)
+    time.sleep(2)
+    image = np.empty((2600, 2000, 3), dtype=np.uint8)
+    camera.capture(image, 'rgb')
+    image = image[:2592, :1952]
