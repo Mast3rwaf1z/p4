ee12301e3f29e7f124c8ce93f8fb7232bcc06a6c
Rasmus Davidsen <rdavid20@student.aau.dk>
2022-04-26 09:43:51 +0200

    updated camera

--- src/camera.py
+++ src/camera.py
@@ -4,8 +4,8 @@ from time import sleep
 camera = PiCamera()
 camera.resolution =(2592,1944)
 
-camera.start_preview()
+#camera.start_preview()
 for i in range(5):
     sleep(2)
     camera.capture('/home/pi/Desktop/testhalløj%s.jpeg' % i)
-camera.stop_preview()
+#camera.stop_preview()
