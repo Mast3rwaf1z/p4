95cc0f8b365501bd7bde8c8322c7629f7ae02cde
rdavid20 <73649411+rdavid20@users.noreply.github.com>
2022-04-27 13:23:06 +0200

    Update camera.py

--- src/modules/camera.py
+++ src/modules/camera.py
@@ -4,24 +4,27 @@ import picamera
 import picamera.array
 import numpy as np
 
-def capturePhoto():
+res = (2592, 1944) # Default Resolution
+
+def capturePhoto(resolution = res):
     camera = picamera.PiCamera()
-    camera.resolution =(2592,1944)
+    camera.resolution = resolution
     dt = datetime.datetime.now(timezone.utc)
     utc_time = dt.replace(tzinfo=timezone.utc)
     utc_timestamp = utc_time.timestamp()
-    camera.capture('/home/pi/Desktop/%s.jpeg' % utc_timestamp)
-    return '/home/pi/Desktop/%s.jpeg' % utc_timestamp
+    location = '/home/pi/Desktop/%s.jpeg' % utc_timestamp
+    camera.capture(location)
+    return location
 
 def captureRawData():
     with picamera.PiCamera() as camera:
-        camera.resolution = (2592, 1944)
+        camera.resolution = res
         image = np.empty((2600, 2000, 3), dtype=np.uint8)
         dt = datetime.datetime.now(timezone.utc)
         utc_time = dt.replace(tzinfo=timezone.utc)
         utc_timestamp = utc_time.timestamp()
         camera.capture(image, 'rgb')
-        image = image[:2592, :1944]
+        image = image[:res[1], :res[2]]
         return image, utc_timestamp
 
 if __name__ == "__main__":
