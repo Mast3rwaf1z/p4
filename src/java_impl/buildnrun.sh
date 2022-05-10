4dccdbbe0c4496221566fd39340c3f2d28b3041e
Mast3rwaf1z <thomasmollerjensen@live.dk>
2022-05-11 00:43:49 +0200

    added removing script to keep working directory clean

--- src/java_impl/buildnrun.sh
+++ src/java_impl/buildnrun.sh
@@ -1,2 +1,5 @@
+rm -r java_impl/build/
+rm -r java_impl/*.class
+
 javac -cp java_impl:java_impl/libs/opencv.jar -d java_impl/build java_impl/*.java
 java -cp java_impl/build:java_impl/libs/opencv.jar java_impl.subsystem
