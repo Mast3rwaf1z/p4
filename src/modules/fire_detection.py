5e1a5c22dff5de8c91ea754bfc1364d86aad474f
Susanne Miranda Aalestrup <saales20@student.aau.dk>
2022-05-13 10:57:25 +0200

    added comments and deleted stuff we didn't use

--- src/modules/fire_detection.py
+++ src/modules/fire_detection.py
@@ -25,103 +25,57 @@ def process_line(values:tuple[np.ndarray]):
 def process_line_ir(values:tuple[np.ndarray]):
     return np.array([255 if values[2][i] > 200 else 0 for i in range(len(values[0]))])
 
-def detect_fire(image:cv.Mat, color_type:str) -> tuple[bool, np.ndarray, tuple[int, int, float]]:
-    #image = cv.imread("wildfire.jpg")
-    color_type = color_type.lower()
-    if color_type == "hsl":
-        hsl = cv.cvtColor(image, cv.COLOR_BGR2HLS) # Converting BGR color scheme to HSV
-        # defining lower mask (red has lower hue values 0-10)
-        lower_red0 = np.array([0,50,50]) # Hue, saturation, value
+def detect_fire(image:cv.Mat, color_type:str) -> tuple[bool, np.ndarray, tuple[int, int, float]]:   # defining image input: image name, color space, setting return data types
+    color_type = color_type.lower()                                                                 # color_type set to not case-sensitive
+    if color_type == "hsl":                                              
+        hsl = cv.cvtColor(image, cv.COLOR_BGR2HLS)                                                  # converting BGR to HSL color scheme (Hue, saturation, lightness)
+        lower_red0 = np.array([0,50,50])                                                            # defining lower mask values (red has lower hue values 0-10)
         upper_red0 = np.array([10,255,255])
-        #upper mask (red has hue values 170-180)
-        lower_red1 = np.array([170,50,50])
+        lower_red1 = np.array([170,50,50])                                                          # defining upper mask values (red has upper hue values 170-180)
         upper_red1 = np.array([180,255,255])
-        mask0 = cv.inRange(hsl, lower_red0, upper_red0) # Threshold the HSV image to get only blue colors
-        mask1 = cv.inRange(hsl, lower_red1, upper_red1)
-        mask = mask0+mask1
-        tmp = cv.bitwise_and(hsl, hsl, mask=mask) # Bitwise-AND mask and original image
-        result = np.array([[tmp[i][j][0] for j in range(len(tmp[i]))] for i in range(len(tmp))])
-    elif color_type == "hsv":
-        hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
-        lower_red = np.array([0,50,50])
-        upper_red = np.array([10,255,255])
-        mask = cv.inRange(hsv, lower_red, upper_red)
-        result = cv.bitwise_and(image, image, mask=mask) 
-    elif color_type == "rgb":
-        blue  = get_channel(image, 0)
-        green = get_channel(image, 1)
-        red   = get_channel(image, 2)
-        result = np.array([[255 if red[i][j] > 165 and green[i][j] < 100 and blue[i][j] < 100 else 0 for j in range(len(image[i]))] for i in range(len(image))])
-    elif color_type == "threaded_rgb":
-        threads:list[Thread] = list()
-        results = np.array([np.ndarray, np.ndarray, np.ndarray])
-        threads.append(Thread(target=return_var, args=(lambda: get_channel(image, 0), results, 0,)))
-        threads.append(Thread(target=return_var, args=(lambda: get_channel(image, 1), results, 1,)))
-        threads.append(Thread(target=return_var, args=(lambda: get_channel(image, 2), results, 2,)))
-        for thread in threads:
-            thread.start()
-        for thread in threads:
-               thread.join()
-        result = np.array([[255 if results[2][i][j] > 165 and results[1][i][j] < 100 and results[0][i][j] < 100 else 0 for j in range(len(image[i]))] for i in range(len(image))])
-    elif color_type == "pool_rgb":
-        with Pool(3) as p:
-            results = p.map(get_channel_from_index, [(image, 0),(image, 1),(image, 2)])
-        with Pool(4) as p:
+        mask0 = cv.inRange(hsl, lower_red0, upper_red0)                                             # masking color range with threshold for the lower mask values
+        mask1 = cv.inRange(hsl, lower_red1, upper_red1)                                             # masking color range with threshold for the upper mask values
+        mask = mask0+mask1                                                                          # defining complete mask thresholds
+        tmp = cv.bitwise_and(hsl, hsl, mask=mask)                                                   # Bitwise-and: Calculates the per-element bit-wise conjunction of two arrays or an array and a scalar.  
+        result = np.array([[tmp[i][j][0] for j in range(len(tmp[i]))] for i in range(len(tmp))])    # Converting OpenCV array to NumPy array 
+    elif color_type == "rgb":                                                             
+        blue  = get_channel(image, 0)                                                               # extracting blue color channel
+        green = get_channel(image, 1)                                                               # extracting green color channel
+        red   = get_channel(image, 2)                                                               # extracting red color channel
+        #below: setting red values to white (value 255) & everything else to black (value 0)                                                                                    
+        result = np.array([[255 if red[i][j] > 165 and green[i][j] < 100 and blue[i][j] < 100 else 0 for j in range(len(image[i]))] for i in range(len(image))]) 
+    elif color_type == "pool_rgb":                                                                  # multiprocessing function
+        with Pool(3) as p:                                                                          # Using 3 cores, starting 3 processes with pool
+            results = p.map(get_channel_from_index, [(image, 0),(image, 1),(image, 2)])             # Using pool.map() to extracting RGB color channels in parallel 
+        with Pool(4) as p:                                                                          # Using 4 cores, starting 4 processes with pool
+            #below: for each color channel, list comprehension to set red values to white (value 255) & everything else to black (value 0)
             result = np.array(p.map(process_line, [(results[0][i], results[1][i], results[2][i]) for i in range(len(image))]))
 
-    all_pixels = sum([len(result[i]) for i in range(len(result))])
-    red_pixels = np.count_nonzero(result)
-    percentage = round(red_pixels * 100 / all_pixels, 2)
+    all_pixels = sum([len(result[i]) for i in range(len(result))])                                  # Getting total amount of pixels
+    red_pixels = np.count_nonzero(result)                                                           # Finding total amount of red pixels by finding non-zero values 
+    percentage = round(red_pixels * 100 / all_pixels, 2)                                            # Finding percentage of red pixels in the image  
 
 
-    #if there are any white pixels on mask, sum will be > 0
-    red_detected = np.sum(result)
-    #yellow_detected = np.sum(mask2)
-    data = (all_pixels, red_pixels, percentage)
-    if red_detected > 0 and percentage < 1:
+    # If there are any white pixels in array, sum will be > 0
+    red_detected = np.sum(result)                                                                   # Finding sum of result 
+    data = (all_pixels, red_pixels, percentage)                                                     # Defining data variable
+    if red_detected > 0 and percentage < 1:                                                         # If sum of matrix is over 0 and percentage of red pixels is over 1:
         #print('Potential fire detected!')
-        return True, result, data
-    else: 
+        return True, result, data                                                                   # Return boolean value True if sum is over 0
+    else:   
         #print('No fire detected..')
-        return False, result, data
-
-def detect_fire_ir(rgb_image:cv.Mat, ir_image:cv.Mat):
-    with Pool(3) as p:
-        ir_results = p.map(get_channel_from_index, [(ir_image, 0),(ir_image, 1),(ir_image, 2)])
-    with Pool(4) as p:
-        ir_result = np.array(p.map(process_line_ir, [(ir_results[0][i], ir_results[1][i], ir_results[2][i]) for i in range(len(ir_image))]))
-
-    with Pool(3) as p:
-        rgb_results = p.map(get_channel_from_index, [(rgb_image, 0),(rgb_image, 1),(rgb_image, 2)])
-    with Pool(4) as p:
-        rgb_result = np.array(p.map(process_line, [(rgb_results[0][i], rgb_results[1][i], rgb_results[2][i]) for i in range(len(rgb_image))]))
-    
-    result = np.array([[255 if rgb_result[i][j] != 0 and ir_result[i][j] != 0 else 0 for j in range(len(rgb_image[i]))] for i in range(len(rgb_image))])
-    all_pixels = sum([len(result[i]) for i in range(len(result))])
-    red_pixels = np.count_nonzero(result)
-    percentage = round(red_pixels * 100 / all_pixels, 2)
+        return False, result, data                                                                  # Else return boolean value False if sum is not over 0
 
 
-    #if there are any white pixels on mask, sum will be > 0
-    red_detected = np.sum(result)
-    #yellow_detected = np.sum(mask2)
-    data = (all_pixels, red_pixels, percentage)
-    if red_detected > 0 and percentage < 1:
-        #print('Potential fire detected!')
-        return True, result, data
-    else: 
-        #print('No fire detected..')
-        return False, result, data
-
-if __name__ == "__main__":
-    from sys import argv
+if __name__ == "__main__":                                                                          # Defining main function
+    from sys import argv                                                                                                          
     from os import chdir, path
 
-    if len(argv) > 1:
+    if len(argv) > 1:                           
         image = cv.imread(argv[1])
     else:
-        chdir(path.dirname(argv[0])) #change directory to script location
-        image = cv.imread("../images/smallfire.jpg")
+        chdir(path.dirname(argv[0]))                                                                # Change directory to script location
+        image = cv.imread("../images/smallfire.jpg")                                                # Defaulting to test image
     hsl_pre = perf_counter()
     _, result, data = detect_fire(image, "hsl")
     hsl_post = perf_counter()
@@ -142,4 +96,4 @@ if __name__ == "__main__":
     print(f'rgb+pool time:  {rgb_pool_post-rgb_pool_pre}')
     for i in range(len(result)):
         for j in range(len(result[i])):
-            assert rgb[i][j] == pool[i][j]# if true the algorithm is calculating correctly
+            assert rgb[i][j] == pool[i][j]                                                          # if true the algorithm is calculating correctly
