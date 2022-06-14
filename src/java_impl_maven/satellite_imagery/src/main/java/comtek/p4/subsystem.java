76cf90ce22ef216e27a47cbd6aec400e4750b429
Thomas Jensen <thomasmollerjensen@live.dk>
2022-06-14 14:47:24 +0200

    added testing stuff

--- src/java_impl_maven/satellite_imagery/src/main/java/comtek/p4/subsystem.java
+++ src/java_impl_maven/satellite_imagery/src/main/java/comtek/p4/subsystem.java
@@ -6,7 +6,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
 
